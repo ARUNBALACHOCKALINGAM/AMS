12cc3744ea7c5ff7f8da750d62eb058ef9987d00
ARUNBALACHOCKALINGAM <abclingam1@gmail.com>
2021-11-16 07:12:47 +0530

    assigned component fix

--- src/components/Assigned.jsx
+++ src/components/Assigned.jsx
@@ -1,16 +1,24 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState , useContext } from "react"
 import {Card} from "@mui/material"
 import {useParams, useSearchParams} from "react-router-dom"
 import Axios from "axios";
 import UploadIcon from '@mui/icons-material/Upload';
+import Statecontext from '../StateContext'
 
 Axios.defaults.baseUrl="https://ams-api.herokuapp.com/api/"
 
 
 function Assigned() {
 
-  const [data,setData] = useState([]);
+  const [assignmentdata,setData] = useState([]);
   let { course } = useParams();
+
+  const {data} = useContext(Statecontext)
+
+  const [info,setInfo] = data
+  
+
+  
 
   const [state,setState]= useState(null);
 
@@ -20,8 +28,9 @@ function Assigned() {
 
   };
 
-  async function handleClick(){
+  async function handleClick(e){
     // Create an object of formData
+ 
     const formData = new FormData();
     
     // Update the formData object
@@ -30,10 +39,18 @@ function Assigned() {
       state.selectedFile,
       state.selectedFile.name
     );
-    const res=await Axios.post("/assign/upload_file?key=6d2044ad57972d5230f586a829893ba5",state.selectedFile);
+    const res=await Axios.post("/assign/upload_file?key=6d2044ad57972d5230f586a829893ba5",{
+      file: state.selectedFile
+    });
+
+    if(res.data){
+      alert("Uploaded")
+    }
 
   }
 
+
+   
 
   
   
@@ -52,8 +69,8 @@ function Assigned() {
   
   return (
     <>
-    {data.map((element)=>{
-       return( <Card style={{display:"flex"}} key={element.AssignmentID} elevation={5} style={{padding:"20px",marginTop:"30px"}}>
+    {assignmentdata.map((element)=>{
+       return(info.id in element.AssignmentCompleted ? "" : <Card style={{display:"flex"}} key={element.AssignmentID} elevation={5} style={{padding:"20px",marginTop:"30px"}}>
        <h4>{element.AssignmentName}</h4>
        <p>{element.AssignmentDescription}</p>
        <input type="file" onChange={onFileChange}/><UploadIcon onClick={handleClick} style={{backgroundColor:"steelblue",borderRadius:"50%",}}/>
--- src/components/Evaluvate.jsx
+++ src/components/Evaluvate.jsx
@@ -1,13 +1,40 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import {Card} from "@mui/material"
 import VisibilityIcon from '@mui/icons-material/Visibility';
+import {useParams} from "react-router-dom"
+import Axios from 'axios';
+
+Axios.defaults.baseUrl="https://ams-api.herokuapp.com/api/"
+
 
 function Evaluvate() {
+
+  const [data,setData]=useState([]);
+
+  const {course} = useParams();
+
+
+  useEffect(()=>{
+    async function fetchAssigments(){
+      const res= await Axios.post("/assign/course?key=6d2044ad57972d5230f586a829893ba5",{CourseID:course})
+      setData(res.data);
+     }
+     fetchAssigments();
+   },[])
+
+
+
   return (
     <>
-       <Card elevation={5} style={{padding:"20px",marginTop:"30px"}}>
-        <h4>Evaluvate an assignment <VisibilityIcon style={{width:"40px",height:"40px",backgroundColor:"steelblue",borderRadius:"50%",marginLeft:"300px",color:"white",padding:"10px"}}/></h4>
-      </Card>
+       {data.map((element)=>{
+       return(    
+       <Card id={element.AssignmentID} elevation={5} style={{padding:"20px",marginTop:"30px"}}>
+       <h4>{element.AssignmentName}<VisibilityIcon onClick={handleClick} style={{width:"40px",height:"40px",backgroundColor:"steelblue",borderRadius:"50%",marginLeft:"300px",color:"white",padding:"10px"}}/></h4>
+     </Card>
+     )
+     })}
+       
+    
     </>
   )
 }
